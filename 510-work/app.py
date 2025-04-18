ceea866783fe95b0d46b187a3fdb6785811a50be
DianaDing1017 <yuqiao17@uw.edu>
2025-04-18 16:50:39 -0700

    Add project files

---
+++ 510-work/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__, 
+    static_url_path='',
+    static_folder='static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5003) 
